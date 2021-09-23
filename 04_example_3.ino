8d1bd505a3881347a427c54c512360790a4acf40
JW0705 <89959941+JW0705@users.noreply.github.com>
2021-09-23 10:32:10 +0900

    Update 04_example_3.ino

--- 04_example_3.ino
+++ 04_example_3.ino
@@ -1,22 +1,16 @@
 #define PIN_LED 13
-unsigned int count, toggle;
+bool toggle = false;
 
 void setup() {
   pinMode(PIN_LED, OUTPUT);
   Serial.begin(115200);
   
-  Serial.println("Hello World!");
-  count = toggle = 0;
   digitalWrite(PIN_LED, toggle); //LED 끄기
 }
 
 void loop() {
-  Serial.println(++count);
-  toggle = toggle_state(toggle); //toggle LED값
+  toggle = !toggle; //toggle LED값
   digitalWrite(PIN_LED, toggle); //LED 상태 업데이트
   delay(1000);
 }
 
-int toggle_state(int toggle){
-  return toggle;
-}
